8b3cabd7a5e1b988dc1b03efaf4f62278b8501e2
mahfuz380 <almahfuz380@gmail.com>
2021-03-20 20:42:04 +0600

    controller done

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,74 @@ Route::get('/', function () {
 });
 
 
-Route::get('/test', function(){
-    return "Hi !! How are you?";
-});
+// Route::get('/test', function(){
+//     return "Hi !! How are you?";
+// });
 
-Route::get('/about', function(){
+// Route::get('/about', function(){
 
-    return "hi about page";
+//     return "hi about page";
 
-});
+// });
 
-Route::get('/contact', function(){
+// Route::get('/contact', function(){
 
-    return "hi contact page";
+//     return "hi contact page";
 
-});
+// });
 
-Route::get('/post/{id}', function($id){
-return " this is post ". $id ;
-});
+// Route::get('/post/{id}', function($id){
+// return " this is post ". $id ;
+// });
 
-Route::get('/admin/posts/example',array('as'=>'admin.home', function(){
+// Route::get('/admin/posts/example',array('as'=>'admin.home', function(){
 
-    $url = route('admin.home');
-    return "this is naming" . $url;
+//     $url = route('admin.home');
+//     return "this is naming" . $url;
 
-}));
+// }));
 
-Route::get('/admin/posts/naming', array('as'=>'admin.naming', function(){
-    $url = route('admin.naming');
-    return "this is ". $url;
+// Route::get('/admin/posts/naming', array('as'=>'admin.naming', function(){
+//     $url = route('admin.naming');
+//     return "this is ". $url;
 
-}));
+// }));
+
+// Route::middleware(['first', 'second'])->group(function () {
+//     Route::get('/', function () {
+//         // Uses first & second Middleware
+//     });
+
+//     Route::get('user/profile', function () {
+//         // Uses first & second Middleware
+//     });
+// });
+
+// Route::middleware(['first','second'])->group(function(){
+
+//     Route::get('/', function(){
+
+//     });
+
+//     Route::get('/', function(){
+
+//     });
+
+// });
+
+// Route::middleware(['first','second'])->group(function(){
+
+//     Route::get('/put', function(){
+
+//     });
+
+//     Route::put('/put', function(){
+
+//     });
+
+// });
+
+
+// Route::get('/post/{id}', 'PostsController@index');
+
+Route::resource('posts', 'PostsController');
