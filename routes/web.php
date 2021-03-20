3991f3cfc9ad8ca82db3181209eed80ec9f0f0b1
mahfuz380 <almahfuz380@gmail.com>
2021-03-20 16:33:17 +0600

    route done

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test', function(){
+    return "Hi !! How are you?";
+});
+
+Route::get('/about', function(){
+
+    return "hi about page";
+
+});
+
+Route::get('/contact', function(){
+
+    return "hi contact page";
+
+});
+
+Route::get('/post/{id}', function($id){
+return " this is post ". $id ;
+});
+
+Route::get('/admin/posts/example',array('as'=>'admin.home', function(){
+
+    $url = route('admin.home');
+    return "this is naming" . $url;
+
+}));
+
+Route::get('/admin/posts/naming', array('as'=>'admin.naming', function(){
+    $url = route('admin.naming');
+    return "this is ". $url;
+
+}));
